7ce24bd8502b30b1372cf67633819c82ad6cd5a5
Irmin Daniel <irmin12julio@gmail.com>
2020-05-08 19:05:06 -0500

    Modificación de title

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>Librioteca</title>
+    <title>Libriotec</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
