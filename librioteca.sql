6154f32c02ee517835efb2f0c619e2bf970a562f
Irmin Daniel <irmin12julio@gmail.com>
2021-02-24 19:58:09 -0600

    Adecuación a la Tabla Notificación

--- librioteca.sql
+++ librioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3308
--- Tiempo de generación: 28-01-2021 a las 11:27:26
+-- Tiempo de generación: 24-02-2021 a las 19:57:05
 -- Versión del servidor: 8.0.18
 -- Versión de PHP: 7.4.0
 
@@ -50,13 +50,25 @@ DROP TABLE IF EXISTS `notificacion`;
 CREATE TABLE IF NOT EXISTS `notificacion` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
   `user_creacion_id` int(11) NOT NULL,
-  `nombre_usu_pedido` varchar(255) NOT NULL,
+  `nombre_user_creacion` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `user_pedido_id` int(11) NOT NULL,
+  `nombre_usu_pedido` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `nombre_libro` varchar(255) NOT NULL,
-  `status` tinyint(1) NOT NULL,
-  `created_at` timestamp NOT NULL,
   `updated_at` timestamp NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Volcado de datos para la tabla `notificacion`
+--
+
+INSERT INTO `notificacion` (`id`, `user_creacion_id`, `nombre_user_creacion`, `user_pedido_id`, `nombre_usu_pedido`, `nombre_libro`, `updated_at`, `created_at`, `status`) VALUES
+(1, 1, 'Yazmin', 1, 'Yazmin', 'Juego de tronos libro canción de hielo y fuego 1', '2021-02-25 07:53:01', '2021-02-25 07:53:01', 0),
+(2, 1, 'Yazmin', 2, 'Ricardo', 'Juego de tronos libro canción de hielo y fuego 1', '2021-02-25 07:54:13', '2021-02-25 07:54:13', 0),
+(3, 1, 'Yazmin', 1, 'Yazmin', 'Juego de tronos libro canción de hielo y fuego 1', '2021-02-25 07:56:10', '2021-02-25 07:55:48', 2),
+(4, 1, 'Yazmin', 1, 'Yazmin', 'Juego de tronos libro canción de hielo y fuego 1', '2021-02-25 07:56:08', '2021-02-25 07:55:50', 3);
 
 -- --------------------------------------------------------
 
@@ -87,7 +99,7 @@ CREATE TABLE IF NOT EXISTS `publicaciones` (
 --
 
 INSERT INTO `publicaciones` (`id`, `user_id`, `titulo`, `image_path`, `description`, `genero`, `editorial`, `autor`, `fecha_publicacion`, `numero_paginas`, `created_at`, `updated_at`) VALUES
-(1, 1, 'Juego de tronos libro canción de hielo y fuego 1', '161185385641yADQ1wEhL._SX319_BO1,204,203,200_.jpg', 'Juego de tronos es una novela de fantasía escrita por el autor estadounidense George R. R. Martin en 1996 y ganadora del premio Locus a la mejor novela de fantasía en 1997. Se trata de la primera entrega de la serie de gran popularidad Canción de hielo y fuego', 'Fántastica', 'El Patito Juan', 'George R. R. Martin', '1995-08-01 00:00:00', 694, '2021-01-28 17:10:57', '2021-01-28 17:10:57');
+(1, 1, 'Juego de tronos libro canción de hielo y fuego 1', '16142170451591416611libro-1500477714.jpg', 'Juego de tronos es una novela de fantasía escrita por el autor estadounidense George R. R. Martin en 1996 y ganadora del premio Locus a la mejor novela de fantasía en 1997. Se trata de la primera entrega de la serie de gran popularidad Canción de hielo y fuego', 'Fántastica', 'El Patito Juan', 'George R. R. Martin', '1996-02-19 00:00:00', 694, '2021-01-28 17:10:57', '2021-02-25 01:37:25');
 
 -- --------------------------------------------------------
 
@@ -119,21 +131,22 @@ CREATE TABLE IF NOT EXISTS `users` (
   `Telefono` bigint(20) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `image` varchar(255) DEFAULT NULL,
+  `image` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL,
   `Estatus_Activo` tinyint(1) DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `remember_token` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_usuario_role` (`role_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `role_id`, `name`, `ApellidoPaterno`, `ApellidoMaterno`, `nickname`, `Telefono`, `email`, `password`, `image_path`, `Estatus_Activo`, `created_at`, `updated_at`, `remember_token`) VALUES
-(1, NULL, 'Yazmin', 'Dzib', 'Cauich', 'Yaz', 123123, 'yazmin@yazmin.com', '$2y$10$EC9YJgqL3sjLDzQZ1QpzNOe/CGMgxvLYKLm42xeo6D/zQ/f9D9d3G', NULL, NULL, '2021-01-28 17:05:30', '2021-01-28 17:05:30', NULL);
+INSERT INTO `users` (`id`, `role_id`, `name`, `ApellidoPaterno`, `ApellidoMaterno`, `nickname`, `Telefono`, `email`, `password`, `image`, `Estatus_Activo`, `created_at`, `updated_at`, `remember_token`) VALUES
+(1, NULL, 'Yazmin', 'Dzib', 'Cauich', 'Yaz', 123123, 'yazmin@yazmin.com', '$2y$10$EC9YJgqL3sjLDzQZ1QpzNOe/CGMgxvLYKLm42xeo6D/zQ/f9D9d3G', '1614216579Cd4iqinUkAEmcgT.jpg.82d79389f835a8c17388662de03ff1e0.jpg', NULL, '2021-01-28 17:05:30', '2021-02-25 01:29:40', 'LkaPDHIXbmIa2fG2Xow7fxSedqzWwsDXIYsUh5nGSRlV4XAprNgK7tV8FYVW'),
+(2, NULL, 'Ricardo', 'Rodriguez', 'Gongora', 'Richi', 9992746954, 'richi@richi.com', '$2y$10$7PfhQHbSsDzusBKtjj.n9.Z434GZmRNh2xcYMi4qN/nPI3HG1jwna', NULL, NULL, '2021-02-25 01:53:59', '2021-02-25 01:53:59', '1OzCrtA4itxw52RCwcldZhwmGGraotpEqSF48NohbKFeZYPSbxWVpaRQRxk6');
 
 --
 -- Restricciones para tablas volcadas
